62ca686f7b0eaaf3fcbd861738cc8050bd4cb02d
PranavAbhilash <95286753+PranavAbhilash@users.noreply.github.com>
2023-01-29 14:48:19 +0000

    created img

--- components/reduce/reduce.tsx
+++ components/reduce/reduce.tsx
@@ -5,7 +5,11 @@ function reduce(){
     return(
         <>
             <div className={styles.bg}> 
+                <img className={styles.img} src="../public/reduce.png"></img>
+                <div className={styles.text}>
+                    <
 
+                </div>
             </div>
         </>
 
