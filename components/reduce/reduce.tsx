c60a8a5a57c04c31385177acc41a513d0903580b
PranavAbhilash <95286753+PranavAbhilash@users.noreply.github.com>
2023-01-29 14:56:51 +0000

    done header

--- components/reduce/reduce.tsx
+++ components/reduce/reduce.tsx
@@ -6,8 +6,9 @@ function reduce(){
         <>
             <div className={styles.bg}> 
                 <img className={styles.img} src="../public/reduce.png"></img>
-                <div className={styles.text}>
-                    <
+                <div >
+                    <h1 className={styles.header}>How to reduce pollution</h1>
+                    <h3 className={styles.reduc}>Reuse items that are still useful such as bottles instead of disposing them. You can also use cloth bags and steel bottles which are reusable instead of plastic bags and bottles</h3>
 
                 </div>
             </div>
