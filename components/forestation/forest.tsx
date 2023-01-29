94c74efb679c58804f3a9f58e615217e49db2aa4
Akul2127 <108287031+Akul2127@users.noreply.github.com>
2023-01-29 14:55:34 +0000

    txts and changes

--- components/forestation/forest.tsx
+++ components/forestation/forest.tsx
@@ -7,18 +7,19 @@ function Forest(){
         <h1>Forestation</h1>
         <h2>How will it help? </h2>
         </div>
-        <h1 id='rect'>Forestation really aids in
+        <p id='rect'>Forestation really aids in
             increasing the amount of trees
             in a degraded forest and that 
             also means there will be more
             houses for the fauna that 
-            inhabits the forest</h1>
-        <h1 id='rectwo'>As the amount of trees will 
+            inhabits the forest</p>
+        <p id='rectwo'>As the amount of trees will 
         grow the amount of carbon
         dioxide will reduce as well
         which gives us more fresh air 
-        in our atmosphere.</h1>
+        in our atmosphere.</p>
         </>
+
     )
 }
 
